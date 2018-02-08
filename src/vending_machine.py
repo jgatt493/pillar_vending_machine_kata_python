86f294cca6199696c21d76c8548c1e78c20e3128
Gatt <jeremy.gatt@maxionwheels.com>
2018-02-08 12:16:35 -0500

    Added init for vending machine class with data structure implemented

---
+++ src/vending_machine.py
@@ -0,0 +1,14 @@
+"""Coins and Products"""
+
+QUARTER = "quarter"
+DIME = "dime"
+NICKEL = "nickel
+
+COLA = "cola
+CANDY = "candy"
+CHIPS = "chips"
+
+class VendingMachine:
+
+    def __init__(self):
+        self.products = [(COLA, 1.00),(CANDY, 0.65),(CHIPS, 0.50)]
